1cc288dfdf715fa079a7e819518d736fa448f96f
Henrique Eidt <henriqueidt@gmail.com>
2026-01-06 19:19:41 -0300

    create renderer

--- index.js
+++ index.js
@@ -2,3 +2,28 @@ function structureNode(nodeName, attributes, ...args) {
   let children = args.length ? [].concat(...args) : null;
   return { nodeName, attributes, children };
 }
+
+function render(node) {
+  if (node.split) {
+    return document.createTextNode(node);
+  }
+
+  let domEl = document.createElement(node.name);
+
+  let attributes = node.attributes || {};
+  Object.keys(attributes).forEach((k) => {
+    domEl.setAttribute(k, attributes[k]);
+  });
+
+  (node.children || []).forEach((children) =>
+    domEl.appendChild(render(children))
+  );
+
+  return domEl;
+}
+
+let virtualDom = <div className="test-div">hello world!</div>;
+
+let dom = render(virtualDom);
+
+document.body.appendChild(dom);
